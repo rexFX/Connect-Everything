f5b42f686914e5159b847a7fe611fde0e2528ed8
Shivam Karn <shivam3511@gmail.com>
2022-06-24 01:10:18 +0530

    Project Starts

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Home from "./routes/home/home.component";
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,11 @@
+import "./home.styles.scss";
+
+const Home = () => {
+	return (
+		<div className="homescreen">
+			<h1 className="main-heading">Connect Everything</h1>
+		</div>
+	);
+};
+
+export default Home;
