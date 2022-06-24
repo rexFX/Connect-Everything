359d64ca78b660c02f80cc47ebeb7fab3b22147d
Shivam Karn <shivam3511@gmail.com>
2022-06-24 19:07:23 +0530

    added react-player

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,9 +1,12 @@
+import VideoPlayer from "../../components/reactplayer/reactplayer.component";
 import "./home.styles.scss";
 
 const Home = () => {
 	return (
 		<div className="homescreen">
 			<h1 className="main-heading">Connect Everything</h1>
+			<VideoPlayer />
+			<button onClick={() => alert("works")}>Test</button>
 		</div>
 	);
 };
